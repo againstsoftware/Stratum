dfdbd6c02802cef4686ed4a0566b664411c58a80
Miguel Rico Feo <miguelrico2031@gmail.com>
2024-11-23 15:58:32 +0100

    eze pullea

--- Assets/Scripts/Initializer/GameInitializer.cs
+++ Assets/Scripts/Initializer/GameInitializer.cs
@@ -7,7 +7,10 @@ public class GameInitializer : MonoBehaviour
 {
     [SerializeField] private GameConfig _config;
     [SerializeField] private Deck[] _decks;
-    [SerializeField] private bool _isTestScene;
+    
+    public enum GameMode { Match, Tutorial, Test }
+
+    [SerializeField] private GameMode _gameMode;
     private void Awake()
     {
         var gameModel = new GameModel(_config, _decks);
@@ -16,16 +19,33 @@ public class GameInitializer : MonoBehaviour
         
         ServiceLocator.Register<IInteractionSystem>(FindAnyObjectByType<InteractionManager>());
         
-        // ServiceLocator.Register<IRulesSystem>(FindAnyObjectByType<DummyRulesManager>()); //de pega
         ServiceLocator.Register<IRulesSystem>(FindAnyObjectByType<RulesManager>()); 
         
         ServiceLocator.Register<IExecutor>(new EffectExecutor());
         
         ServiceLocator.Register<IView>(FindAnyObjectByType<ViewManager>());
-        ServiceLocator.Register<ITurnSystem>(FindAnyObjectByType<TurnManager>());
-        
-        if(_isTestScene)  ServiceLocator.Register<ICommunicationSystem>(FindAnyObjectByType<TestModeCommunications>());
-        else ServiceLocator.Register<ICommunicationSystem>(FindAnyObjectByType<GameNetwork>());
+
+
+        switch (_gameMode)
+        {
+            case GameMode.Match:
+                ServiceLocator.Register<ITurnSystem>(FindAnyObjectByType<TurnManager>());
+                ServiceLocator.Register<ICommunicationSystem>(FindAnyObjectByType<GameNetwork>());
+                break;
+            
+            case GameMode.Test:
+                ServiceLocator.Register<ITurnSystem>(FindAnyObjectByType<TurnManager>());
+                ServiceLocator.Register<ICommunicationSystem>(FindAnyObjectByType<TestModeCommunications>());
+                break;
+            
+            case GameMode.Tutorial:
+                ServiceLocator.Register<ITurnSystem>(FindAnyObjectByType<TutorialManager>());
+                ServiceLocator.Register<ICommunicationSystem>(FindAnyObjectByType<TutorialManager>());
+                break;
+            
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
 
     }
 
---
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,39 @@
+
+using System;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour, ITurnSystem, ICommunicationSystem
+{
+    public PlayerCharacter PlayerOnTurn { get; }
+    public bool IsAuthority => true;
+    public bool IsRNGSynced => true;
+    
+    public event Action<PlayerCharacter> OnTurnChanged;
+    public event Action<PlayerCharacter> OnActionEnded;
+    
+    public event Action OnGameStart;
+    public void StartGame()
+    {
+        
+        
+        
+        OnGameStart?.Invoke();
+    }
+    
+    public void SendActionToAuthority(PlayerAction action)
+    {
+        ServiceLocator.Get<IExecutor>().ExecutePlayerActionEffects(action);
+    }
+
+    public void EndAction()
+    {
+        
+    }
+
+    
+    
+    
+    public void ChangeTurn(PlayerCharacter playerOnTurn) {}
+    public void SyncRNGs() {}
+    public void SendTurnChange(PlayerCharacter playerOnTurn) {}
+}
